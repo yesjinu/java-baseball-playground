cfc3502ca7d294265937a26d07c7064726f7f6c4
jinu.noh <jinu.noh@bagelcode.com>
2022-01-01 18:22:46 +0900

    add tests for String

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,8 +1,9 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
     @Test
@@ -12,10 +13,38 @@ public class StringTest {
     }
 
     @Test
-    void split() {
+    void splitString() {
         String[] actual = "1,2".split(",");
         assertThat(actual).contains("1");
         assertThat(actual).contains("2");
         assertThat(actual).containsExactly("1", "2");
+    }
+
+    @Test 
+    void splitWithNotExistValue() {
+        String[] actual = "1".split(",");
+        assertThat(actual).containsExactly("1");
+    }
+
+    @Test
+    void substring() {
+        String actual = "(1,2)".substring(1, 4);
+        assertThat(actual).matches("1,2");
+    }
+
+    @Test
+    void charAtValid() {
+        char actual = "abc".charAt(1);
+        assertThat(actual).isEqualTo('b');
+    }
+
+    @Test
+    @DisplayName("nananan")
+    void charAtInvalid() {
+        assertThatThrownBy(() -> "abc".charAt(4))
+                .isInstanceOf(IndexOutOfBoundsException.class);
+
+        // assertThatExceptionOfType(IndexOutOfBoundsException.class)
+        //         .isThrownBy(() -> "abc".charAt(4));
     }
 }
