2d76718d370af580a2c6e6f57b8c7471fa019a9b
jinu.noh <jinu.noh@bagelcode.com>
2022-01-02 15:51:35 +0900

    Rename tests specifically

--- src/test/java/com/jinu/project/CalculatorTest.java
+++ src/test/java/com/jinu/project/CalculatorTest.java
@@ -8,25 +8,25 @@ public class CalculatorTest {
   Calculator calculator = new Calculator();;
 
   @Test
-  void addSimple() throws Exception {
+  void shouldAddTwoNumbers() throws Exception {
     int actual = calculator.operate("1 + 2");
     assertThat(actual).isEqualTo(3);
   }
 
   @Test
-  void addComplicate() throws Exception {
+  void shouldAddThreeNumbers() throws Exception {
     int actual = calculator.operate("1 + 2 + 3");
     assertThat(actual).isEqualTo(6);
   }
   
   @Test
-  void onlyOneInt() throws Exception {
+  void shouldReturnWhenOnlyOneLiteral() throws Exception {
     int actual = calculator.operate("1");
     assertThat(actual).isEqualTo(1);
   }
 
   @Test
-  void testComplicate() throws Exception {
+  void shouldCalculateComplexFormula() throws Exception {
     int actual = calculator.operate("2 + 3 * 4 / 2");
     assertThat(actual).isEqualTo(10);
   }
