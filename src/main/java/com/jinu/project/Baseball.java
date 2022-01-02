fedf540db4750f9b50321d8e23dde5d1d5d2d2c3
jinu.noh <jinu.noh@bagelcode.com>
2022-01-02 15:57:19 +0900

    Initial commit

---
+++ src/main/java/com/jinu/project/Baseball.java
@@ -0,0 +1,5 @@
+package com.jinu.project;
+
+public class Baseball {
+  
+}
---
+++ src/test/java/com/jinu/project/BaseballTest.java
@@ -0,0 +1,8 @@
+package com.jinu.project;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+public class BaseballTest {
+  Baseball baseball = new Baseball();;
+}
