d4b49a52c9d8822855429919638e9ec734b012e6
jinu.noh <jinu.noh@bagelcode.com>
2022-01-02 15:47:35 +0900

    add tests for exceptional cases

--- src/test/java/com/jinu/project/CalculatorTest.java
+++ src/test/java/com/jinu/project/CalculatorTest.java
@@ -1,37 +1,46 @@
 package com.jinu.project;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import java.util.Scanner;
-
+import static org.assertj.core.api.Assertions.*;
 
 public class CalculatorTest {
   Calculator calculator = new Calculator();;
 
   @Test
-  void addSimple() {
+  void addSimple() throws Exception {
     int actual = calculator.operate("1 + 2");
     assertThat(actual).isEqualTo(3);
   }
 
   @Test
-  void addComplicate() {
+  void addComplicate() throws Exception {
     int actual = calculator.operate("1 + 2 + 3");
     assertThat(actual).isEqualTo(6);
   }
   
   @Test
-  void onlyOneInt() {
+  void onlyOneInt() throws Exception {
     int actual = calculator.operate("1");
     assertThat(actual).isEqualTo(1);
   }
 
   @Test
-  void testComplicate() {
+  void testComplicate() throws Exception {
     int actual = calculator.operate("2 + 3 * 4 / 2");
     assertThat(actual).isEqualTo(10);
+  }
+
+  @Test
+  void shouldBeNumberFormat() {
+    assertThatThrownBy(() -> calculator.operate("invalid"))
+        .isInstanceOf(NumberFormatException.class);
+  }
+
+  @Test
+  void shouldBeValidFormula() {
+    assertThatThrownBy(() -> calculator.operate("1 + "))
+        .isInstanceOf(Exception.class)
+        .hasMessageContaining("Invalid");
   }
 }
