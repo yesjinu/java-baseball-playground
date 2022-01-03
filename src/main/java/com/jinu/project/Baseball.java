22d2132835f2f9da258b88e5084aec2ba91fb095
jinu.noh <jinu.noh@bagelcode.com>
2022-01-03 20:40:53 +0900

    feat: implement strike and ball logic

--- src/main/java/com/jinu/project/Baseball.java
+++ src/main/java/com/jinu/project/Baseball.java
@@ -11,7 +11,43 @@ public class Baseball {
         return this.answer;
     }
 
-    
+    public int[] guess(String assumption) {
+        int strikeCount = this.countStrike(assumption);
+        int ballCount = this.countBall(assumption);
 
+        return new int[] { strikeCount, ballCount };
+    }
+    
+    private boolean isStrike(String answer, char candidate, int index) {
+        return answer.charAt(index) == candidate;
+    }
+
+    private boolean isBall(String answer, char candidate, int index) {
+        System.out.println(answer.indexOf(candidate));
+        System.out.println(index);
+        return (answer.indexOf(candidate) != -1) && (answer.indexOf(candidate) != index);
+    }
+    
+    private int countStrike(String assumption) {
+        int count = 0;
+        for (int i = 0; i < 3; i++) {
+            if (this.isStrike(answer, assumption.charAt(i), i)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    private int countBall(String assumption) {
+        int count = 0;
+        for (int i = 0; i < 3; i++) {
+            if (this.isBall(answer, assumption.charAt(i), i)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
 
 }
--- src/test/java/com/jinu/project/BaseballTest.java
+++ src/test/java/com/jinu/project/BaseballTest.java
@@ -1,10 +1,11 @@
 package com.jinu.project;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import static org.assertj.core.api.Assertions.*;
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
 
 public class BaseballTest {
     String answer = "123";
@@ -16,10 +17,44 @@ public class BaseballTest {
         assertThat(actual).isEqualTo(answer);
     }
 
-    // @Test
-    // void shouldBeNothing() {
-    //     String assumption = "456";
-    //     baseball.guess(assumption);
-    // }
+    @Test
+    void shouldNothingMatched() {
+        String assumption = "456";
+        int[] result = baseball.guess(assumption);
+        int[] actual = { 0, 0 };
+        assertTrue(Arrays.equals(result, actual));
+    }
+
+    @Test
+    void shouldOneBall() {
+        String assumption = "451";
+        int[] result = baseball.guess(assumption);
+        int[] actual = { 0, 1 };   
+        assertTrue(Arrays.equals(result, actual));
+    }
+
+    @Test
+    void shouldOneStrike() {
+        String assumption = "145";
+        int[] result = baseball.guess(assumption);
+        int[] actual = { 1, 0 };
+        assertTrue(Arrays.equals(result, actual));
+    }
+    
+    @Test
+    void shouldTwoBall() {
+        String assumption = "412";
+        int[] result = baseball.guess(assumption);
+        int[] actual = { 0, 2 };
+        assertTrue(Arrays.equals(result, actual));
+    }
+    
+    @Test
+    void shouldOneStrikeOneBall() {
+        String assumption = "142";
+        int[] result = baseball.guess(assumption);
+        int[] actual = { 1, 1 };
+        assertTrue(Arrays.equals(result, actual));
+    }
 
 }
