e4f6fde8661340d435a30906cf95642f4746ae55
jinu.noh <jinu.noh@bagelcode.com>
2022-01-03 21:02:32 +0900

    refactor: divide function into small pieces

--- src/main/java/com/jinu/project/Baseball.java
+++ src/main/java/com/jinu/project/Baseball.java
@@ -3,37 +3,56 @@ package com.jinu.project;
 public class Baseball {
     private String answer;
 
-    public Baseball(String input) {
+    Baseball(String input) {
         this.answer = input;
     }
 
-    public String getAnswer() {
+
+    String getAnswer() {
         return this.answer;
     }
 
-    public int[] guess(String assumption) {
+    
+    int[] guess(String assumption) {
         int strikeCount = this.countStrike(assumption);
         int ballCount = this.countBall(assumption);
 
         return new int[] { strikeCount, ballCount };
     }
     
-    private boolean isStrike(String answer, char candidate, int index) {
+
+    private int strike(String answer, char candidate, int index) {
+        if (sameValueOnSamePlace(answer, candidate, index)) {
+            return 1;
+        }
+
+        return 0;
+    }
+
+    
+    private int ball(String answer, char candidate, int index) {
+        if (sameValueOnWrongPlace(answer, candidate, index)) {
+            return 1;
+        }
+        
+        return 0;
+    }
+    
+
+    private boolean sameValueOnSamePlace(String answer, char candidate, int index) {
         return answer.charAt(index) == candidate;
     }
 
-    private boolean isBall(String answer, char candidate, int index) {
-        System.out.println(answer.indexOf(candidate));
-        System.out.println(index);
+
+    private boolean sameValueOnWrongPlace(String answer, char candidate, int index) {
         return (answer.indexOf(candidate) != -1) && (answer.indexOf(candidate) != index);
     }
-    
+
+
     private int countStrike(String assumption) {
         int count = 0;
         for (int i = 0; i < 3; i++) {
-            if (this.isStrike(answer, assumption.charAt(i), i)) {
-                count++;
-            }
+            count += this.strike(answer, assumption.charAt(i), i);
         }
 
         return count;
@@ -42,9 +61,7 @@ public class Baseball {
     private int countBall(String assumption) {
         int count = 0;
         for (int i = 0; i < 3; i++) {
-            if (this.isBall(answer, assumption.charAt(i), i)) {
-                count++;
-            }
+            count += this.ball(answer, assumption.charAt(i), i);
         }
 
         return count;
