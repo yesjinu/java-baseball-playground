3fb69e16396ac9e2cddbbeea6857051c1aa77010
jinu.noh <jinu.noh@bagelcode.com>
2022-01-03 19:14:10 +0900

    feat: implement constructor for Baseball class and tests

--- src/main/java/com/jinu/project/Baseball.java
+++ src/main/java/com/jinu/project/Baseball.java
@@ -1,5 +1,17 @@
 package com.jinu.project;
 
 public class Baseball {
+    private String answer;
+
+    public Baseball(String input) {
+        this.answer = input;
+    }
+
+    public String getAnswer() {
+        return this.answer;
+    }
+
     
+
+
 }
--- src/test/java/com/jinu/project/BaseballTest.java
+++ src/test/java/com/jinu/project/BaseballTest.java
@@ -1,8 +1,25 @@
 package com.jinu.project;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
 import static org.assertj.core.api.Assertions.*;
 
 public class BaseballTest {
-    Baseball baseball = new Baseball();;
+    String answer = "123";
+    Baseball baseball = new Baseball(answer);
+
+    @Test
+    void shouldReturnAnswer() {
+        String actual = baseball.getAnswer();
+        assertThat(actual).isEqualTo(answer);
+    }
+
+    // @Test
+    // void shouldBeNothing() {
+    //     String assumption = "456";
+    //     baseball.guess(assumption);
+    // }
+
 }
