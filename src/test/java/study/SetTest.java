c64ac87bd0a752093f44dffe6c0ad8ab93d30308
Jinu Noh <yes.jinu@gmail.com>
2022-05-16 21:38:37 +0900

    test: Implement Set test when throw exception

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,52 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+    }
+
+    @Test
+    void size() {
+        assertThat(numbers.size()).isEqualTo(4);
+    }
+
+    @Test
+    void contains() {
+        assertThat(numbers.contains(1)).isTrue();
+        assertThat(numbers.contains(2)).isTrue();
+        assertThat(numbers.contains(3)).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void contains(int number) {
+        assertThat(numbers.contains(number)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:True", "2:True", "3:True", "5:False"}, delimiter = ':')
+    void contains(String strNumber, String strBoolean) {
+        Integer num = Integer.parseInt(strNumber);
+        Boolean bool = Boolean.parseBoolean(strBoolean);
+        assertThat(numbers.contains(num)).isEqualTo(bool);
+    }
+}
