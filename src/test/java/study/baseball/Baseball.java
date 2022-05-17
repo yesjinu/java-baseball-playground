7604fc8325f446d8de59242cf277a8217d0950a9
Jinu Noh <yes.jinu@gmail.com>
2022-05-17 23:00:32 +0900

    tdd: make tests pass

--- src/test/java/study/baseball/Baseball.java
+++ src/test/java/study/baseball/Baseball.java
@@ -10,18 +10,39 @@ public class Baseball {
     public String guess(String guess) {
         // iterate each character of guess
         int strikeCount = 0;
+        int ballCount = 0;
         
-        for (int i = 0; i < guess.length(); i++) {
+        for (int i = 0; i < 3; i++) {
             char guessChar = guess.charAt(i);
-            char answerChar = ANSWER.charAt(i);
+            for (int j = 0; j < 3; j++) {
+                char answerChar = ANSWER.charAt(j);
 
-            if (guessChar == answerChar) {
-                strikeCount += 1;
+                if (guessChar == answerChar && i == j) {
+                    strikeCount += 1;
+                    break;
+                } else if (guessChar == answerChar) {
+                    ballCount += 1;
+                    break;
+                }
             }
         }
 
-        if (strikeCount > 0) return strikeCount + " Strike";
+        String result = "";
+        if (strikeCount > 0) {
+            result = result + strikeCount + " Strike";
+        }
 
-        return "nothing";
+        if (ballCount > 0) {
+            if (!result.isEmpty()) {
+                result += " ";
+            }
+            result = result + ballCount + " Ball";
+        }
+
+        if (strikeCount == 0 && ballCount == 0) {
+            return "nothing";
+        }
+
+        return result;
     }
 }
