092bdda1398494adb7c3d3bd0982fc165c14e1c0
Jinu Noh <yes.jinu@gmail.com>
2022-05-17 23:00:20 +0900

    tdd: update tests that fail

--- src/test/java/study/baseball/BaseballTest.java
+++ src/test/java/study/baseball/BaseballTest.java
@@ -13,13 +13,12 @@ public class BaseballTest {
     void checkStrike() {
         String result1 = game.guess("123");
         String result2 = game.guess("132");
-        String result3 = game.guess("453");
+        String result3 = game.guess("253");
         String result4 = game.guess("456");
 
         assertThat(result1).isEqualTo("3 Strike");
-        assertThat(result2).isEqualTo("1 Strike");
-        assertThat(result3).isEqualTo("1 Strike");
+        assertThat(result2).isEqualTo("1 Strike 2 Ball");
+        assertThat(result3).isEqualTo("1 Strike 1 Ball");
         assertThat(result4).isEqualTo("nothing");
     }
-
 }
